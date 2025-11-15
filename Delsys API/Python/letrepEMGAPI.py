ffd19f8e62832958ab65e21af80ad7a085c67e82
Gilon Kraft <gilonkraft@letu.edu>
2025-11-15 16:34:08 -0600

    EMG API implementation. Main goal is to gather an understanding of how the API works and resolve initial errors.

---
+++ Delsys API/Python/letrepEMGAPI.py
@@ -0,0 +1,17 @@
+# Gilon Kraft
+# LETREP26 - Software Subteam
+# Date Started: 11/14/2025
+
+"""This program implements the Delsys API
+First, it establishes a connection with the EMG sensors"""
+
+import sys
+from AeroPy.TrignoBase import TrignoBase
+
+# Creating an instance of the Trigno EMG Base
+base = TrignoBase()
+TrigBase = base.BaseInstance
+
+# Connecting to the Trigno USB
+ValidateBase(key, license)
+print(key)
