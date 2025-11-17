47e5d19163b437a96bf2573fed3ba54e75e71d78
Gilon Kraft <gilonkraft@letu.edu>
2025-11-17 15:58:19 -0600

    Processed the data

--- PiezoSampleScaler.py
+++ PiezoSampleScaler.py
@@ -52,10 +52,10 @@ else:
         print(f"\nAn unexpected error occurred: {e}")
 
 # Showing a Graph of the Force Data
-plt.plot(averaged_scaling)
+plt.plot(scaled_data)
 plt.show()
 
 # Generating a filename for scaled data and saving data to csv
 file_name = os.path.basename(file_path)
 file_name = "scaled_" + file_name 
-np.savetxt(file_name, averaged_scaling, delimiter=',', fmt='%d')
+np.savetxt(file_name, scaled_data, delimiter=',', fmt='%d')
